323cd889463e809b266ca4f8b1597abdd2543ef7
Abdelzaher11 <oabdelzaher68@gmail.com>
2021-09-27 00:07:22 +0200

    version1.0.1

--- lib/constants/controllers.dart
+++ lib/constants/controllers.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
 
+///login screen controllers
+
+
 var kUsernameLogInController = TextEditingController();
 var kPasswordLogInController = TextEditingController();
--- lib/screens/log_in_screen.dart
+++ lib/screens/log_in_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:news/constants/colors.dart';
 import 'package:news/constants/controllers.dart';
 import 'package:news/constants/variables/font_size.dart';
@@ -15,7 +16,6 @@ class _LogInScreenState extends State<LogInScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       //backgroundColor: kBlackColor,
       body: BackgroundWidget(
           Column(
@@ -32,141 +32,245 @@ class _LogInScreenState extends State<LogInScreen> {
             ),
             child: Padding(
               padding: EdgeInsets.all(kPadding5),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                // ignore: prefer_const_literals_to_create_immutables
-                children: [
-                  ///title
-                  Text(
-                    'Login',
-                    style: TextStyle(
-                      color:kGreenColor,
-                      fontSize: kFont18,
-                      fontWeight: FontWeight.bold,
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    ///title
+                    Text(
+                      'Login',
+                      style: TextStyle(
+                        color:kGreenColor,
+                        fontSize: kFont18,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  ///lines
-                  const SizedBox(height: 8.0,),
-                  // ignore: sized_box_for_whitespace
-                  Container(
-                    width: MediaQuery.of(context).size.width*0.18,
-                    height: MediaQuery.of(context).size.height*0.002,
-                    //color: kBlackColor,
-                    child: Row(
-                      // ignore: prefer_const_literals_to_create_immutables
+                    ///lines
+                    const SizedBox(height: 8.0,),
+                    // ignore: sized_box_for_whitespace
+                    Container(
+                      width: MediaQuery.of(context).size.width*0.18,
+                      height: MediaQuery.of(context).size.height*0.002,
+                      //color: kBlackColor,
+                      child: Row(
+                        // ignore: prefer_const_literals_to_create_immutables
+                        children: [
+                          Expanded(
+                            child: Column(
+                              // ignore: prefer_const_literals_to_create_immutables
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    color: kBlackColor,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(width: 10.0,),
+                          Expanded(
+                            child: Column(
+                              // ignore: prefer_const_literals_to_create_immutables
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    color: kBlackColor,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(width: 10.0,),
+                          Expanded(
+                            child: Column(
+                              // ignore: prefer_const_literals_to_create_immutables
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    color: kBlackColor,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    ///input fields
+                    Padding(
+                      padding: EdgeInsets.all(kPadding20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ///username title
+                          Text(
+                            'Username',
+                            style: TextStyle(
+                              color: kGreenColor,
+                              fontSize: kFont16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 5.0,),
+                          ///username controller
+                          Container(
+                            clipBehavior: Clip.antiAliasWithSaveLayer,
+                            decoration: BoxDecoration(
+                              color: kWhiteColor,
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(25.0),
+                              ),
+                            ),
+                            child: TextField(
+                              controller: kUsernameLogInController,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                contentPadding: EdgeInsets.only(left: kPadding15,),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 15.0,),
+                          ///password title
+                          Text(
+                            'Password',
+                            style: TextStyle(
+                              color: kGreenColor,
+                              fontSize: kFont16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 5.0,),
+                          ///password controller
+                          Container(
+                            clipBehavior: Clip.antiAliasWithSaveLayer,
+                            decoration: BoxDecoration(
+                              color: kWhiteColor,
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(25.0),
+                              ),
+                            ),
+                            child: TextField(
+                              controller: kPasswordLogInController,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                contentPadding: EdgeInsets.only(left: kPadding15,),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    ///login button
+                    MaterialButton(
+                      onPressed: (){},
+                      child: Icon(Icons.arrow_forward_ios,color: kWhiteColor,size: 30.0,),
+                      color: kGreenColor.withOpacity(0.7),
+                    ),
+                    const SizedBox(height: 15.0,),
+                    ///social media icon
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Expanded(
-                          child: Column(
-                            // ignore: prefer_const_literals_to_create_immutables
-                            children: [
-                              Expanded(
-                                child: Container(
-                                  color: kBlackColor,
-                                ),
-                              ),
-                            ],
+                        ///facebook icon
+                        InkWell(
+                          onTap: (){},
+                          child: CircleAvatar(
+                            backgroundColor: Colors.blue[900],
+                            radius: 28.0,
+                            child: const FaIcon(FontAwesomeIcons.facebookF,size: 40.0,),
                           ),
                         ),
-                        const SizedBox(width: 10.0,),
-                        Expanded(
-                          child: Column(
-                            // ignore: prefer_const_literals_to_create_immutables
-                            children: [
-                              Expanded(
-                                child: Container(
-                                  color: kBlackColor,
-                                ),
-                              ),
-                            ],
+                        const SizedBox(width: 10,),
+                        ///google icon
+                        InkWell(
+                          onTap: (){},
+                          child: CircleAvatar(
+                            backgroundColor: Colors.red,
+                            radius: 28.0,
+                            child: const FaIcon(FontAwesomeIcons.googlePlusG,size: 40.0,),
                           ),
                         ),
-                        const SizedBox(width: 10.0,),
-                        Expanded(
-                          child: Column(
-                            // ignore: prefer_const_literals_to_create_immutables
-                            children: [
-                              Expanded(
-                                child: Container(
-                                  color: kBlackColor,
-                                ),
-                              ),
-                            ],
+                        const SizedBox(width: 10,),
+                        ///discord icon
+                        InkWell(
+                          onTap: (){},
+                          child: const CircleAvatar(
+                            backgroundColor: Colors.purple,
+                            radius: 28.0,
+                            child: FaIcon(FontAwesomeIcons.discord,size: 40.0,),
                           ),
                         ),
                       ],
                     ),
-                  ),
-                  ///input fields
-                  Padding(
-                    padding: EdgeInsets.all(kPadding20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    ///or and sign up button
+                    Text(
+                      'or',
+                      style: TextStyle(
+                        fontSize: kFont17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width*0.15,
+                      height: MediaQuery.of(context).size.height*0.004,
+                      color: Colors.black,
+                    ),
+                    InkWell(
+                      onTap: (){},
+                      child: Text(
+                        'Sign up',
+                        style: TextStyle(
+                          color: kGreenColor,
+                          fontSize: kFont19,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const Divider(height: 50.0,color: Colors.transparent,),
+                    ///aboutUs, contactUs And privacy button
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        ///username title
-                        Text(
-                          'Username',
-                          style: TextStyle(
-                            color: kGreenColor,
-                            fontSize: kFont16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(height: 5.0,),
-                        ///username controller
-                        Container(
-                          clipBehavior: Clip.antiAliasWithSaveLayer,
-                          decoration: BoxDecoration(
-                            color: kWhiteColor,
-                            borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(25.0),
-                            ),
-                          ),
-                          child: TextField(
-                            controller: kUsernameLogInController,
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              contentPadding: EdgeInsets.only(left: kPadding15,),
+                        ///AboutUs
+                        InkWell(
+                          onTap: (){},
+                          child: Text(
+                            'AboutUs-',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: kFont15,
                             ),
                           ),
                         ),
-                        const SizedBox(height: 15.0,),
-                        ///password title
-                        Text(
-                          'Password',
-                          style: TextStyle(
-                            color: kGreenColor,
-                            fontSize: kFont16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(height: 5.0,),
-                        ///password controller
-                        Container(
-                          clipBehavior: Clip.antiAliasWithSaveLayer,
-                          decoration: BoxDecoration(
-                            color: kWhiteColor,
-                            borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(25.0),
-                            ),
-                          ),
-                          child: TextField(
-                            controller: kPasswordLogInController,
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              contentPadding: EdgeInsets.only(left: kPadding15,),
+                        ///ContactUs
+                        InkWell(
+                          onTap: (){},
+                          child: Text(
+                            'ContactUs-',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: kFont15,
                             ),
                           ),
                         ),
+                        ///Privacy
+                        InkWell(
+                          onTap: (){},
+                          child: Text(
+                            'Privacy',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: kFont15,
+                            ),
+                          ),
+                        ),
+
                       ],
                     ),
-                  ),
-                  ///login button
-                  MaterialButton(
-                    onPressed: (){},
-                    child: Icon(Icons.arrow_forward_ios,color: kWhiteColor,size: 30.0,),
-                    color: kGreenColor,
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
